cc105b7ff256afe1f0df46b57b8c90e57000ea2d
mohiuddinmh <mohiuddinmh@gmail.com>
2022-11-14 22:45:49 -0600

    artItem tests

--- src/components/ArtItem/index.test.tsx
+++ src/components/ArtItem/index.test.tsx
@@ -12,47 +12,103 @@ test('an art item is fetched and displayed correctly', async () => {
 })
 
 test('an error message is displayed if the api call fails', async () => {
+	// given
 	renderWithQueryProvider(
 		<>
 			<ArtItem id={1} />
 			<ToastContainer />
 		</>
 	)
+
+	// when
 	server.use(rest.get('https://api.artic.edu/api/v1/artworks/:id', (req, res, ctx) => {
 		return res(
 			ctx.status(400),
 		)
 	}))
+
+	// then
 	await waitFor(() => expect(screen.getByText(/Unable to find an art for id 1/)).toBeInTheDocument())
 })
 
 test('for an art item, submit button is disabled until a rating is selected', async () => {
-	renderWithQueryProvider(<ArtItem id={1} />)
-
-	await waitFor(() => expect(screen.getByText('Plate One from Collection of Various Vases')).toBeInTheDocument())
-})
-
-test('for an art item, clicking numbered button updates rating display below image to be that number', async () => {
+	// Given
 	renderWithQueryProvider(<ArtItem id={1} />)
 	await waitFor(() => expect(screen.getByText('Plate One from Collection of Various Vases')).toBeInTheDocument())
 
 	const ratingScale = screen.getByTestId('rating-stars')
 	const ratingOneButton = ratingScale.querySelector('[value=\'1\']') as Element
+
+	// then
+	expect(screen.getByText(/Submit/)).toHaveAttribute('disabled')
+
+	// when
+	fireEvent.click(ratingOneButton)
+
+	// then
+	expect(screen.getByText(/Submit/)).not.toHaveAttribute('disabled')
+	expect(1).toBeTruthy()
+})
+
+test('for an art item, clicking numbered button updates rating display below image to be that number', async () => {
+	// given
+	renderWithQueryProvider(<ArtItem id={1} />)
+	await waitFor(() => expect(screen.getByText('Plate One from Collection of Various Vases')).toBeInTheDocument())
+	const ratingScale = screen.getByTestId('rating-stars')
+	const ratingOneButton = ratingScale.querySelector('[value=\'1\']') as Element
+
+	// when
+	fireEvent.click(ratingOneButton)
+
+	// then
+	expect(screen.getByTestId('rating').textContent).toBe('Rating: 1')
+})
+
+test('for an art item, clicking numbered button updates rating display below image to be that number, clicking two different numbers one after the other', async () => {
+	// given
+	renderWithQueryProvider(<ArtItem id={1} />)
+	await waitFor(() => expect(screen.getByText('Plate One from Collection of Various Vases')).toBeInTheDocument())
+	const ratingScale = screen.getByTestId('rating-stars')
+	const ratingOneButton = ratingScale.querySelector('[value=\'1\']') as Element
 	const ratingTwoButton = ratingScale.querySelector('[value=\'2\']') as Element
 
+	// when
 	fireEvent.click(ratingOneButton)
+
+	// then
 	expect(screen.getByTestId('rating').textContent).toBe('Rating: 1')
 
+	// when
 	fireEvent.click(ratingTwoButton)
+
+	// then
 	expect(screen.getByTestId('rating')).toHaveTextContent('Rating: 2')
 })
 
+test('for an art item, clicking submit POSTs update, displays a toast success message, hides buttons', async () => {
+	// given
+	server.use(rest.post('https://20e2q.mocklab.io/rating', (req, res, ctx) => {
+		return res(
+			ctx.status(200),
+		)
+	}))
+	renderWithQueryProvider(
+		<>
+			<ArtItem id={1} />
+			<ToastContainer />
+		</>
+	)
+	await waitFor(() => expect(screen.getByText('Plate One from Collection of Various Vases')).toBeInTheDocument())
+	const ratingScale = screen.getByTestId('rating-stars')
+	const ratingOneButton = ratingScale.querySelector('[value=\'1\']') as Element
+	const submitButton = screen.getByText(/Submit/)
+	screen.debug()
 
-// TODO
-// test('for an art item, clicking numbered button updates rating display below image to be that number, clicking two different numbers one after the other', () => {
-// })
-//
-// test('for an art item, clicking submit POSTs update, displays a toast success message, hides buttons', () => {
-// 	// The endpoint and payload for the submit button can be found in the submit method in `App.tsx`.
-// 	// For the purpose of this test, please use a mock function instead.
-// })
+	// when
+	fireEvent.click(ratingOneButton)
+	fireEvent.click(submitButton)
+
+	// then
+	await waitFor(() => expect(screen.getByText(/Rating submitted for/)).toBeInTheDocument())
+})
+
